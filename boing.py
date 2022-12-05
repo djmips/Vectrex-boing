f21f99fc6270f0bcba7abb0e995da6c56e2e682b
djmips <david.c.galloway@gmail.com>
2022-12-05 01:47:57 -0800

    Generate all data about the ball directly into include files

--- boing.py
+++ boing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from pathlib import Path
 
 sLat = 180/8
 sLong = 180/8
@@ -17,142 +18,152 @@ rotAngle = -22.5
 cosra = cos(rotAngle)
 sinra = sin(rotAngle)
 
+data_folder = Path("BoingSrc/source/")
+
 def drawCircumference():
 
-    x1 = 0
-    y1 = radius
+    latfile = data_folder / "circ.h"
+    
+    with open(latfile, 'w') as f:
 
-    rx1 = x1 * cosra - y1 * sinra
-    ry1 = x1 * sinra + y1 * cosra
+        print ("const signed char ball[] = {", file=f)
+        print ("15,", file=f)
 
-    for deg in np.arange (0.0,360.0,sLong):
-        x2 = sin(deg + sLong) * radius
-        y2 = cos(deg + sLong) * radius
-      
-        # rotate
-        rx2 = x2 * cosra - y2 * sinra
-        ry2 = x2 * sinra + y2 * cosra
+        x1 = 0
+        y1 = radius
+
+        rx1 = x1 * cosra - y1 * sinra
+        ry1 = x1 * sinra + y1 * cosra
+
+        for deg in np.arange (0.0,360.0,sLong):
+            x2 = sin(deg + sLong) * radius
+            y2 = cos(deg + sLong) * radius
         
-        dx = (rx2 - rx1)
-        dy = (ry2 - ry1)
+            # rotate
+            rx2 = x2 * cosra - y2 * sinra
+            ry2 = x2 * sinra + y2 * cosra
+            
+            dx = (rx2 - rx1)
+            dy = (ry2 - ry1)
 
-        print(str(int(dy)) +  ",", str(int(dx * aspectConst)) + "," )  # Full globe if using 360
+            print(str(int(dy)) +  ",", str(int(dx * aspectConst)) + ",", file=f )  # Full globe if using 360
 
-        rx1 = rx2
-        ry1 = ry2
+            rx1 = rx2
+            ry1 = ry2
+
+        print ("};", file=f)
 
 def drawLatitude():
 
-    scaledRadius = radius / 3.85
+    latfile = data_folder / "lat.h"
+    
+    with open(latfile, 'w') as f:
 
-    latSeg = 0
+        scaledRadius = radius / 3.85
 
-    for longDeg in np.arange(sLong, 180.0, sLong ):
+        latSeg = 0
 
-        print ("const signed char latSeg" + str(latSeg) + "[] = {")
-        print ("0,")        
+        for longDeg in np.arange(sLong, 180.0, sLong ):
 
-        x1 = 0
-        y1 = scaledRadius
+            print ("const signed char latSeg" + str(latSeg) + "[] = {", file=f)
+            print ("0,", file=f)        
 
-        x2 = sin(longDeg) * scaledRadius
-        y2 = cos(longDeg) * scaledRadius
+            x1 = 0
+            y1 = scaledRadius
 
-        rx1 = x1 * cosra - y1 * sinra
-        ry1 = x1 * sinra + y1 * cosra
+            x2 = sin(longDeg) * scaledRadius
+            y2 = cos(longDeg) * scaledRadius
 
-        rx2 = x2 * cosra - y2 * sinra
-        ry2 = x2 * sinra + y2 * cosra
+            rx1 = x1 * cosra - y1 * sinra
+            ry1 = x1 * sinra + y1 * cosra
 
-        dx = (rx2 - rx1)
-        dy = (ry2 - ry1)
+            rx2 = x2 * cosra - y2 * sinra
+            ry2 = x2 * sinra + y2 * cosra
 
-        print(str(int(dy)) +  ",", str(int(dx * aspectConst)) + "," )
+            dx = (rx2 - rx1)
+            dy = (ry2 - ry1)
 
-        doffset = (latSeg+1) * 45
-        x1 = sin(longDeg-doffset) * scaledRadius
-        y1 = cos(longDeg-doffset) * scaledRadius
+            print(str(int(dy)) +  ",", str(int(dx * aspectConst)) + ",", file=f )
 
-        rx1 = x1 * cosra - y1 * sinra
-        ry1 = x1 * sinra + y1 * cosra
+            doffset = (latSeg+1) * 45
+            x1 = sin(longDeg-doffset) * scaledRadius
+            y1 = cos(longDeg-doffset) * scaledRadius
 
-        rx2 = x2 * cosra - y2 * sinra
-        ry2 = x2 * sinra + y2 * cosra
+            rx1 = x1 * cosra - y1 * sinra
+            ry1 = x1 * sinra + y1 * cosra
 
-        dx = (rx1 - rx2)
-        dy = (ry1 - ry2)
+            rx2 = x2 * cosra - y2 * sinra
+            ry2 = x2 * sinra + y2 * cosra
 
-        print(str(int(dy)) +  ",", str(int(dx * aspectConst)) + "," )
+            dx = (rx1 - rx2)
+            dy = (ry1 - ry2)
 
+            print(str(int(dy)) +  ",", str(int(dx * aspectConst)) + ",", file=f )
 
-        #dx = (x2 - x1)
-        #dy = (y2 - y1)
-        #print(str(int(dy)) +  ",", str(int(dx * aspectConst)) + "," )
-
-        #line(x1,y1, -x1,y1)
-
-        print ("};")
-        latSeg = latSeg + 1
+            print ("};", file=f)
+            latSeg = latSeg + 1
 
 def drawLongitude():
 
     #latDeg = 0
     seg = 0
 
-    for ani in range(0, 22, 2):
+    latfile = data_folder / "long.h"
+    
+    with open(latfile, 'w') as f:
 
-        print ("const signed char long" + str(seg) + "[] = {")
-        print ("55,")
+        for ani in range(0, 22, 2):
 
-        negF = 1
+            print ("const signed char long" + str(seg) + "[] = {", file=f)
+            print ("55,", file=f)
 
-        for latDeg in np.arange((ani + (sLat/2)), (180.0 - sLat/2), sLat ):
+            negF = 1
 
-           
-            if (negF > 0):
-                for deg in np.arange(0.0, (180.001-sLong), sLong):
-                    x1 = cos(latDeg) * (radius * sin(deg))
-                    y1 = cos(deg) * radius
-                    
-                    x2 = cos(latDeg) * (radius * sin(deg + sLong))
-                    y2 = cos(deg+sLat) * radius
+            for latDeg in np.arange((ani + (sLat/2)), (180.0 - sLat/2), sLat ):
 
-                    rx1 = x1 * cosra - y1 * sinra
-                    ry1 = x1 * sinra + y1 * cosra
+            
+                if (negF > 0):
+                    for deg in np.arange(0.0, (180.001-sLong), sLong):
+                        x1 = cos(latDeg) * (radius * sin(deg))
+                        y1 = cos(deg) * radius
+                        
+                        x2 = cos(latDeg) * (radius * sin(deg + sLong))
+                        y2 = cos(deg+sLat) * radius
 
-                    rx2 = x2 * cosra - y2 * sinra
-                    ry2 = x2 * sinra + y2 * cosra
+                        rx1 = x1 * cosra - y1 * sinra
+                        ry1 = x1 * sinra + y1 * cosra
 
-                    dx = (rx2 - rx1)
-                    dy = (ry2 - ry1)
+                        rx2 = x2 * cosra - y2 * sinra
+                        ry2 = x2 * sinra + y2 * cosra
 
-                    print(str(int(dy)) +  ",", str(int(dx * aspectConst)) + "," )
-            else:
-                for deg in np.arange((180 - sLong),-0.001 , -sLong):
-                    x1 = cos(latDeg) * (radius * sin(deg))
-                    y1 = cos(deg) * radius
-                    
-                    x2 = cos(latDeg) * (radius * sin(deg + sLong))
-                    y2 = cos(deg+sLat) * radius
+                        dx = (rx2 - rx1)
+                        dy = (ry2 - ry1)
 
-                    rx1 = x1 * cosra - y1 * sinra
-                    ry1 = x1 * sinra + y1 * cosra
+                        print(str(int(dy)) +  ",", str(int(dx * aspectConst)) + ",", file=f)
+                else:
+                    for deg in np.arange((180 - sLong),-0.001 , -sLong):
+                        x1 = cos(latDeg) * (radius * sin(deg))
+                        y1 = cos(deg) * radius
+                        
+                        x2 = cos(latDeg) * (radius * sin(deg + sLong))
+                        y2 = cos(deg+sLat) * radius
 
-                    rx2 = x2 * cosra - y2 * sinra
-                    ry2 = x2 * sinra + y2 * cosra
+                        rx1 = x1 * cosra - y1 * sinra
+                        ry1 = x1 * sinra + y1 * cosra
 
-                    dx = (rx1 - rx2)
-                    dy = (ry1 - ry2)
+                        rx2 = x2 * cosra - y2 * sinra
+                        ry2 = x2 * sinra + y2 * cosra
 
-                    print(str(int(dy)) +  ",", str(int(dx * aspectConst)) + "," )
+                        dx = (rx1 - rx2)
+                        dy = (ry1 - ry2)
 
-            negF = -negF
+                        print(str(int(dy)) +  ",", str(int(dx * aspectConst)) + ",", file=f)
 
-        seg = seg + 1
-        print ("};")
+                negF = -negF
 
+            seg = seg + 1
+            print ("};", file=f)
 
-
-#drawCircumference()
+drawCircumference()
 drawLatitude()
-#drawLongitude()
+drawLongitude()
