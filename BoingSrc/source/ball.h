014f70eef850e223485695b32aca46e2bf7afcfe
djmips <david.c.galloway@gmail.com>
2022-12-04 17:17:11 -0800

    Moved circumference definition to it's own file from ball.h to circ.h

--- BoingSrc/source/ball.h
+++ BoingSrc/source/ball.h
@@ -9,27 +9,7 @@
 #define SPRITE_SIZE 25
 #define MAX_ANIM 11
 
-const signed char ball[] = {
-                            15,
--43, 77,
--64, 52,
--76, 18,
--76, -18,
--64, -52,
--43, -77,
--15, -91,
-15, -91,
-43, -77,
-64, -52,
-76, -18,
-76, 18,
-64, 52,
-43, 77,
-15, 91,
--15, 91,
-};
-
-
+#include "circ.h"
 #include "lat.h"
 #include "long.h"
 
